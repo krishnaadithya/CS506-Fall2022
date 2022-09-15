1adb6dfb31a1757c260790b998ad2b928bd3cf42
krishnaadithya <krishnavadithya@gmail.com>
2022-09-15 13:21:23 -0400

    tree updated

--- labs/01-lab/infrastructure/tree.py
+++ labs/01-lab/infrastructure/tree.py
@@ -1,3 +1,25 @@
+from tkinter import N
+
+
+def make_triange(n):
+     for i in range(n):
+            for j in range(n-i):
+                print(' ', end=' ')
+            for k in range(2*i+1):
+                print('*',end=' ')
+            print() 
+
+def make_bark(n):
+    for i in range(n):
+        for j in range(n-1):
+            print(' ', end=' ')
+        print('* * *')
+
+
 def draw_tree():
-    print("tree not found")
+    n=10
+    make_triange(n)
+    make_triange(n)
+    make_bark(n)
+    
     return
