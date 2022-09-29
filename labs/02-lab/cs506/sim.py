ac2b5c46b899b7932446e86665becd1a4a9311c7
Krishna Adithya V <krishnavadithya@gmail.com>
2022-09-29 13:14:20 -0400

    Updated read.py and sim.py

--- labs/02-lab/cs506/sim.py
+++ labs/02-lab/cs506/sim.py
@@ -1,3 +1,6 @@
+import numpy as np
+from numpy.linalg import norm
+
 def euclidean_dist(x, y):
     res = 0
     for i in range(len(x)):
@@ -5,12 +8,27 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+
+    return return sum(abs(val1-val2) for val1, val2 in zip(x,y))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+
+    if np.unique(x) == [0,1] and np.unique(y) == [0,1]:
+    
+        intersection = np.logical_and(x, y)
+        union = np.logical_or(x, y)
+        similarity = intersection.sum() / float(union.sum())
+        distance = 1- similarity
+    
+    else: 
+        intersection = len(list(set(x).intersection(y)))
+        union = (len(x) + len(y)) - intersection
+        distance = float(intersection) / union
+
+    return distance
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+
+    return np.dot(A,B)/(norm(A)*norm(B))
 
 # Feel free to add more
